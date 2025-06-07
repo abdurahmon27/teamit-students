fc5ceeab7d568b1c0993e88c512c4d1463cf9fd2
Abdurahmon <bekzotovich12@gmail.com>
2025-06-07 16:55:18 +0500

    init commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div className="w-full mx-auto h-screen flex flex-col items-center justify-center text-5xl font-semibold text-blue-50 bg-gray-800 py-5">
+      <span className="my-auto">Hello World</span>
+      <footer className="mt-auto font-thin text-2xl">
+        made by <a href="https://teamit.uz" className="underline">teamit.uz</a>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
