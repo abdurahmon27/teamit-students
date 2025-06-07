708d11762aa5504cea547492c2a148c13663ae08
Abdurahmon <bekzotovich12@gmail.com>
2025-06-07 17:04:46 +0500

    fix: text

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 function App() {
   return (
-    <div className="w-full mx-auto h-screen flex flex-col items-center justify-center text-5xl font-semibold text-blue-50 bg-gray-800 py-5">
-      <span className="my-auto">Hello World</span>
+    <div className="w-full mx-auto h-screen flex flex-col items-center justify-center text-5xl font-semibold text-blue-200 bg-gray-900 py-5">
+      <span className="my-auto">Wassup Nigga!</span>
       <footer className="mt-auto font-thin text-2xl">
         made by <a href="https://teamit.uz" className="underline">teamit.uz</a>
       </footer>
